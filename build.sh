0b695ac250b47610f9de2ac4027f0a8af28b2f10
André Bauer <monotek@users.noreply.github.com>
2022-05-05 22:19:21 +0200

    test (#2)
    * test build
    
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -eux
 
+export CGO_ENABLED=0
+#export GOBIN=/conntrack-stats-exporter
+export GOOS=linux
+
+
+if [ "$(lscpu | grep Architecture | awk '{print $2}')" = "aarch64" ]; then 
+    echo "arm64"
+    export GOARCH=arm64
+else 
+    echo "amd64"
+    export GOARCH=arm64
+fi
+
 go mod verify 
 go test ./...
-ARCH=$(lscpu | grep Architecture | awk '{print $2}')
-PLATFORM=$(if [ "$ARCH" = "aarch64" ]; then echo "arm64"; else echo "amd64"; fi)
-GOOS=linux GOARCH=$PLATFORM CGO_ENABLED=0 go build --ldflags="-X pkg.version=$(git describe --dirty)"
+#go build
+
+go build --ldflags="-X pkg.version=$(git describe --dirty)"
