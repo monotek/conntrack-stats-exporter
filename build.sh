d2339cb905545b033e59ea71e4b9e400c3edb840
André Bauer <andre.bauer@staffbase.com>
2022-05-05 22:05:00 +0200

    test
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -eux
 
-# ARCH=$(lscpu | grep Architecture | awk '{print $2}')
-# PLATFORM=$(if [ "$ARCH" = "aarch64" ]; then echo "arm64"; else echo "amd64"; fi)
-# GOOS=linux 
-# GOARCH=$PLATFORM 
-# CGO_ENABLED=1
+export GOOS='linux'
+export CGO_ENABLED='0'
 
-#GOOS=linux GOARCH=$PLATFORM CGO_ENABLED=0 go build --ldflags="-X pkg.version=$(git describe --dirty)"
+if [ "$(lscpu | grep Architecture | awk '{print $2}')" = "aarch64" ]; then 
+    echo "arm64"
+    export GOARCH=arm64
+else 
+    echo "amd64"
+    export GOARCH=arm64
+fi
+
 go mod verify 
 go test ./...
-# go build --ldflags="-X pkg.version=$(git describe --dirty)"
 go build
+
+# go build --ldflags="-X pkg.version=$(git describe --dirty)"
